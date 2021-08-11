d1be5197f152f2f28763918ffa1c1cadbc1b6252
zhengjiuzhe <1597516368@qq.com>
2021-08-11 23:53:07 +0800

    change izip to zip

--- recognition/arcface_torch/pre-processing/mtcnn_detector.py
+++ recognition/arcface_torch/pre-processing/mtcnn_detector.py
@@ -6,9 +6,8 @@ import math
 import cv2
 from multiprocessing import Pool
 from itertools import repeat
-from itertools import izip
+from itertools import zip
 from helper import nms, adjust_input, generate_bbox, detect_first_stage_warpper
-
 
 class MtcnnDetector(object):
     """
@@ -347,9 +346,9 @@ class MtcnnDetector(object):
             total_boxes = []
             for batch in sliced_index:
                 # local_boxes = self.Pool.map( detect_first_stage_warpper, \
-                #        izip(repeat(img), self.PNets[:len(batch)], [scales[i] for i in batch], repeat(self.threshold[0])) )
+                #        zip(repeat(img), self.PNets[:len(batch)], [scales[i] for i in batch], repeat(self.threshold[0])) )
                 local_boxes = map(detect_first_stage_warpper, \
-                                  izip(repeat(img), self.PNets[:len(batch)], [scales[i] for i in batch],
+                                  zip(repeat(img), self.PNets[:len(batch)], [scales[i] for i in batch],
                                        repeat(self.threshold[0])))
                 total_boxes.extend(local_boxes)
 
