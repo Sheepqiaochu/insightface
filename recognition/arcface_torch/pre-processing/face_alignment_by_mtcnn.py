e9d30dde48ff88317107f68f6ee5a24bd3bcf8c7
zhengjiuzhe <1597516368@qq.com>
2021-08-11 17:33:26 +0800

    revise face_path

--- recognition/arcface_torch/pre-processing/face_alignment_by_mtcnn.py
+++ recognition/arcface_torch/pre-processing/face_alignment_by_mtcnn.py
@@ -12,6 +12,7 @@ import cv2
 import os
 import time
 import sys
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../../..', 'src', 'common'))
 import face_image
 import face_preprocess
@@ -19,16 +20,16 @@ import numpy as np
 
 mtcnn_path = os.path.join(os.path.dirname(__file__), 'mtcnn-model')
 
-detector = MtcnnDetector(model_folder=mtcnn_path, ctx=mx.cpu(0), num_worker = 4 , accurate_landmark = False)
+detector = MtcnnDetector(model_folder=mtcnn_path, ctx=mx.cpu(0), num_worker=4, accurate_landmark=False)
 
-faces_path = r'F:\datasets\20_faces'          # 人脸数据文件夹
-output_path = r'F:\datasets\20_faces_clip'    # 对齐后的保存的人脸数据文件夹
+faces_path = r'~/datasets/images'  # 人脸数据文件夹
+output_path = r'~/datasets/images_training'  # 对齐后的保存的人脸数据文件夹
 
-for root,_,files in os.walk(faces_path):
+for root, _, files in os.walk(faces_path):
     for fname in files:
-        img = cv2.imread(os.path.join(root,fname))
+        img = cv2.imread(os.path.join(root, fname))
         # new_root = root.replace('20_faces','20_faces_clip')
-        new_root = os.path.join(output_path,os.path.basename(root))
+        new_root = os.path.join(output_path, os.path.basename(root))
         if not os.path.exists(new_root):
             os.mkdir(new_root)
         # run detector
