522d3e4ba7e5840a085f2e79bcaa0b9968755804
zhengjiuzhe <1597516368@qq.com>
2021-08-14 02:58:54 +0800

    revise crop methods

--- recognition/arcface_torch/pre-processing/resize.py
+++ recognition/arcface_torch/pre-processing/resize.py
@@ -1,14 +1,14 @@
 import os
-
+import numpy
 import cv2
 from PIL import Image
 from torchvision import transforms
 
-# faces_path = r'/data/users/yangqiancheng/datasets\images'  # 人脸数据文件夹
-# output_path = r'/data/users/yangqiancheng/datasets\cropped'  # 对齐后的保存的人脸数据文件夹
+faces_path = r'/data/users/yangqiancheng/datasets\images'  # 人脸数据文件夹
+output_path = r'/data/users/yangqiancheng/datasets\cropped'  # 对齐后的保存的人脸数据文件夹
 
-faces_path = r'/data/users/yangqiancheng/datasets/images'  # 人脸数据文件夹
-output_path = r'/data/users/yangqiancheng/datasets/cropped'  # 对齐后的保存的人脸数据文件夹
+# faces_path = r'/data/users/yangqiancheng/datasets/images'  # 人脸数据文件夹
+# output_path = r'/data/users/yangqiancheng/datasets/cropped'  # 对齐后的保存的人脸数据文件夹
 
 for root, _, files in os.walk(faces_path):
     for fname in files:
@@ -20,7 +20,7 @@ for root, _, files in os.walk(faces_path):
         cropped = transforms.Compose([
             transforms.ToPILImage(),
             transforms.CenterCrop(112)])
-        cropped_image = cropped(img)
+        cropped_image = cv2.cvtColor(numpy.asarray(cropped(img)),cv2.COLOR_RGB2BGR)
         cv2.imwrite(os.path.join(new_root, fname), cropped_image)
         # cv2.imshow("img", img)
         # cv2.waitKey(0)
